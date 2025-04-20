609f1e032c8f31ae6b122b44451a88173c48aad6
shinokun <hypatia.tile02021@gmail.com>
2025-04-20 13:35:01 +0900

    add lsh (tiny shell implemented by C)

---
+++ experiments/lsh.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+#define LSH_BUFSIZE 1024
+#define LSH_TOK_BUFSIZE 64
+#define LSH_TOK_DELIM " \t\r\n\a"
+
+char *lsh_read_line(void) {
+  char *line = NULL;
+  ssize_t bufsize = 0;
+  getline(&line, &bufsize, stdin);
+  return line;
+}
+
+char **lsh_split_line(char *line) {
+  int bufsize = LSH_TOK_BUFSIZE, position = 0;
+  char **tokens = malloc(bufsize * sizeof(char*));
+  char *token;
+
+  token = strtok(line, LSH_TOK_DELIM);
+  while (token != NULL) {
+
+    // Copy the pointer to the current token into the tokens array at the current position.
+    // The pointer itself is stored, not the string data it points to.
+    tokens[position++] = token;
+
+    if (position >= bufsize) {
+      bufsize += LSH_TOK_BUFSIZE;
+      tokens = realloc(tokens, bufsize * sizeof(char*));
+    }
+
+    token = strtok(NULL, LSH_TOK_DELIM);
+  }
+  tokens[position] = NULL;
+  return tokens;
+}
+
+int lsh_launch(char **args) {
+  pid_t pid, wpid;
+  int status;
+
+  pid = fork();
+  if (pid == 0) {
+    // Child process
+    if (execvp(args[0], args) == -1) {
+      perror("lsh");
+    }
+    exit(EXIT_FAILURE);
+
+  } else if (pid < 0) {
+    // Error forking
+    perror("lsh");
+  } else {
+    do {
+      wpid = waitpid(pid, &status, WUNTRACED);
+    } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+  }
+
+  return 1;
+}
+
+int lsh_execute(char **args) {
+  if (args[0] == NULL) return 1; // Empty command
+
+  if (strcmp(args[0], "exit") == 0) return 0;
+  if (strcmp(args[0], "cd") == 0) {
+    if (args[1] == NULL) {
+      fprintf(stderr, "lsh: cd: missing argument\n");
+    } else {
+      if (chdir(args[1]) != 0) perror("lsh");
+    }
+    return 1;
+  }
+
+  return lsh_launch(args);
+}
+
+void lsh_loop(void) {
+  char *line;
+  char **args;
+  int status;
+
+  do {
+    printf("> ");
+    line = lsh_read_line();
+    args = lsh_split_line(line);
+    status = lsh_execute(args);
+
+    free(line);
+    free(args);
+  } while (status);
+}
+
+int main() {
+  lsh_loop();
+  return EXIT_SUCCESS;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
